71033eb0fc17fb2a8258404f1e98067fe594c625
omitogunjesufemi <omitogunjesufemi@gmail.com>
2025-04-14 22:24:44 +0100

    Added a README.md file and Adjusted the background color of the navbar

---
+++ README.md
@@ -0,0 +1,15 @@
+## About The Profiles Project
+
+
+### Built With
+This section list the major frameworks and libraries used to bootstrap the Profiles project.
+
+* [![React][React.js]][React-url]
+* [![Bootstrap][Bootstrap.com]][Bootstrap-url]
+
+## Getting Started
+### Prerequisites
+### Installation
+
+
+## Usage
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import Button from "./Button.jsx";
 function Navbar () {
     return (
         <>
-            <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+            <nav className='navbar navbar-expand-lg bg-light'>
                 <form className='container-fluid' role='search'>
-                    <div className='input-group'>
-                        <a className='navbar-brand'>PROFILE</a>
-                        <input className='form-control me-2' type='search' placeholder='e.g. JayMikee' aria-label='Search'/>
-                        <Button color={'outline-success'} type={'submit'}>
-                            Search
-                        </Button>
+                    <div className='container-sm d-inline-flex'>
+                        <a className='navbar-brand'>Profile</a>
+                        <div className='input-group'>
+                            <input className='form-control me-2' type='search' placeholder='e.g. JayMikee' aria-label='Search'/>
+                            <Button color={'outline-success'} type={'submit'}>
+                                Search
+                            </Button>
+                        </div>
                     </div>
                 </form>
             </nav>
