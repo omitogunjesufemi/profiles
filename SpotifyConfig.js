f5ead46ee0a29bd293dc40d54ab4654658f84a1c
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2025-04-29 22:33:43 +0100

    Update SpotifyConfig.js

--- SpotifyConfig.js
+++ SpotifyConfig.js
@@ -1,5 +1,5 @@
-export const SPOTIFY_CLIENT_ID = '59348d9fc1f84574834913342b45816a';
-export const SPOTIFY_CLIENT_SECRET = '9adfeba861394a39bb2dffb7d24e56cc';
+export const SPOTIFY_CLIENT_ID = '';
+export const SPOTIFY_CLIENT_SECRET = '';
 
 export const SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token';
 export const SPOTIFY_API_BASE  = 'https://api.spotify.com/v1';
